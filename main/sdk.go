0a133f906523638ebc83ac7aa38857aa9f24a149
lkx <linkexu12@163.com>
2023-03-22 17:09:10 +0800

    copy from 50wTag.txt
    Signed-off-by: lkx <linkexu12@163.com>

--- main/sdk.go
+++ main/sdk.go
@@ -18,7 +18,7 @@ type Log struct {
 	Request   string `json:"request"`
 }
 
-func GetLinesFromFileAndJson(filename string, index int) []Log { //
+func GetLinesFromFileAndJson(filename string, index int) []Log {
 	var logdata = make([]Log, 0)
 	file, err := os.Open(filename)
 	if err != nil {
@@ -51,52 +51,51 @@ func GetLinesFromFileAndJson(filename string, index int) []Log { //
 	return logdata
 }
 
-func main() {
-	//logdata := GetLinesFromFileAndJson("/mnt/sdc/data/clv_data/one_billion_all.txt", 1000000000)
-	logdata := GetLinesFromFileAndJson("../resource/50w.txt", 500000)      //  ../resource/50w.txt  /mnt/sdc/data/clv_data/one_billion_01.txt
-	host, err := url.Parse(fmt.Sprintf("http://%s:%d", "127.0.0.1", 8086)) // /write?db=clv&u=admin&p=At1314comi!
+func copy_from_50w() {
+	logdata := GetLinesFromFileAndJson("../resource/50w.txt", 500000)
+	host, err := url.Parse(fmt.Sprintf("http://%s:%d", "127.0.0.1", 8086))
 	if err != nil {
 		log.Fatal(err)
 	}
 	con, err := client.NewClient(client.Config{URL: *host, Username: "admin", Password: "At1314comi!"})
 	if err != nil {
 		log.Fatal(err)
+		panic(err)
 	}
 
 	q := client.Query{
 		Command: "drop database clv; create database clv",
 	}
+
 	if r, err := con.Query(q); err != nil {
 		fmt.Println("create database error:", r.Err)
 		log.Fatal(err)
 	}
 
 	q = client.Query{
-		Command:  "create measurement dlvTable(logs string field, index idx1 logs type text)",
+		Command:  "create measurement clvTable(logs string field, index idx1 logs type text)",
 		Database: "clv",
 	}
+
 	if r, err := con.Query(q); err != nil {
-		fmt.Println("create measurment error:", r.Err)
+		fmt.Println("create measurment(clv.clvTable) error:", r.Err)
 		log.Fatal(err)
+		panic(err)
 	}
 
 	s := time.Now().UnixMicro()
-
 	k := 0
 	i := 0
-	for k = 0; k < 5000; k++ { //for k := 0; k < 5000; k++ 10000000
+	for k = 0; k < 5000; k++ {
 		pts := make([]client.Point, 0)
 		var index int64
-		for i = 100 * k; i < 100*k+100; i++ { //for i := 100 * k; i < 100*k+100; i++
+		for i = 100 * k; i < 100*k+100; i++ {
 			pt := client.Point{
 				Measurement: "clvTable",
-				//Tags: map[string]string{
-				// "host": "A" + string(k),
-				//},
 				Fields: map[string]interface{}{
 					"logs": logdata[i].Request,
 				},
-				Time:      time.Unix(0, logdata[i].Timestamp), //  int64(i)
+				Time:      time.Unix(0, logdata[i].Timestamp),
 				Precision: "ns",
 			}
 			pts = append(pts, pt)
@@ -120,4 +119,123 @@ func main() {
 	e := time.Now().UnixMicro()
 	fmt.Println("sdk push logs cost time:")
 	fmt.Println(float64(e-s) / 1000)
+}
+
+type TagLog struct {
+	Timestamp int64  `json:"@timestamp"`
+	Request   string `json:"request"`
+	ClientIP  string `json:"clientip"`
+}
+
+func GetLinesFrom50Tag(filename string, index int) []TagLog {
+	var logdata = make([]TagLog, 0)
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+	buff := bufio.NewReader(file)
+	i := 0
+	for {
+		i++
+		data, _, eof := buff.ReadLine()
+		if eof == io.EOF {
+			break
+		}
+		jsonStr := data
+		var logs TagLog
+		err = json.Unmarshal(jsonStr, &logs)
+		if err != nil {
+			fmt.Println(i)
+			panic("解析失败")
+		}
+		logdata = append(logdata, TagLog{
+			Timestamp: logs.Timestamp,
+			Request:   logs.Request,
+			ClientIP:  logs.ClientIP,
+		})
+		if i == index {
+			break
+		}
+	}
+	return logdata
+}
+
+func copy_from_50wTag() {
+	logdata := GetLinesFrom50Tag("../resource/50wTag.txt", 500000)
+	host, err := url.Parse(fmt.Sprintf("http://%s:%d", "127.0.0.1", 8086))
+	if err != nil {
+		log.Fatal(err)
+	}
+	con, err := client.NewClient(client.Config{URL: *host, Username: "admin", Password: "At1314comi!"})
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	q := client.Query{
+		Command: "drop database clvTag; create database clvTag",
+	}
+
+	if r, err := con.Query(q); err != nil {
+		fmt.Println("create database(clvTag) error:", r.Err)
+		log.Fatal(err)
+		panic(err)
+	}
+
+	q = client.Query{
+		Command:  "create measurement clvTable(clientip string tag, logs string field, index idx1 logs type text)",
+		Database: "clvTag",
+	}
+
+	if r, err := con.Query(q); err != nil {
+		fmt.Println("create measurment(clvTag.clvTable) error:", r.Err)
+		log.Fatal(err)
+		panic(err)
+	}
+
+	s := time.Now().UnixMicro()
+	k := 0
+	i := 0
+	for k = 0; k < 5000; k++ {
+		pts := make([]client.Point, 0)
+		var index int64
+		for i = 100 * k; i < 100*k+100; i++ {
+			pt := client.Point{
+				Measurement: "clvTable",
+				Fields: map[string]interface{}{
+					"logs": logdata[i].Request,
+				},
+				Tags: map[string]string{
+					"clientip": logdata[i].ClientIP,
+				},
+				Time:      time.Unix(0, logdata[i].Timestamp),
+				Precision: "ns",
+			}
+			pts = append(pts, pt)
+			index++
+		}
+		if k%10000 == 0 {
+			fmt.Println("current time：", time.Now(), "write(w): ", k)
+		}
+		bps := client.BatchPoints{
+			Points:   pts,
+			Database: "clvTag",
+		}
+		for retry := 0; retry < 5; retry++ {
+			_, err := con.Write(bps)
+			if err == nil {
+				break
+			}
+			fmt.Println("current time：", time.Now(), "panic error, (k,i, retry):", k, i, retry, " error: ", err)
+		}
+	}
+	e := time.Now().UnixMicro()
+	fmt.Println("sdk push logs cost time:")
+	fmt.Println(float64(e-s) / 1000)
+}
+
+func main() {
+	copy_from_50w()
+	copy_from_50wTag()
 }
